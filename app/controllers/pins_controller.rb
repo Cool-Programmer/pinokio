8100ed6cd17fa6a651ec3812973414ca837219d5
= <margaryan.mher.28@gmail.com>
2016-11-27 10:50:03 +0400

    Styling done

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -8,11 +8,11 @@ class PinsController < ApplicationController
   end
 
   def new
-    @pin = cureent_user.pins.build
+    @pin = current_user.pins.build
   end
 
   def create
-    @pin = cureent_user.pins.build(pin_params)
+    @pin = current_user.pins.build(pin_params)
     if @pin.save
       redirect_to @pin, notice:'Your pin created successfully. Yay!'
     else
